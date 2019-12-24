6c9dd52144e706610c3ad280e7fde31f7323f44d
Andrew Kane <andrew@chartkick.com>
2019-12-24 02:19:07 -0500

    Added test for invalid dimensions

--- test/onnxruntime_test.rb
+++ test/onnxruntime_test.rb
@@ -120,4 +120,11 @@ class OnnxRuntimeTest < Minitest::Test
     output = model.predict({input: [[5.8, 2.8]]}, output_names: ["label"])
     assert_equal ["label"], output.keys
   end
+
+  def test_invalid_dimensions
+    model = OnnxRuntime::Model.new("test/support/model.onnx")
+    assert_raises OnnxRuntime::Error do
+      model.predict(x: [1])
+    end
+  end
 end
