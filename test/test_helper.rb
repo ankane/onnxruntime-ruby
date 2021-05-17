b676febd12e4d1f65565ce73df4d0ea49d92640e
Andrew Kane <andrew@ankane.org>
2021-05-17 15:15:08 -0700

    Simplify stress code [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,22 +7,12 @@ require "tmpdir"
 class Minitest::Test
   private
 
-  def setup
-    GC.stress = true if stress?
-  end
-
-  def teardown
-    GC.stress = false if stress?
-  end
-
   def assert_elements_in_delta(expected, actual)
     assert_equal expected.size, actual.size
     expected.zip(actual) do |exp, act|
       assert_in_delta exp, act
     end
   end
-
-  def stress?
-    ENV["STRESS"]
-  end
 end
+
+GC.stress = ENV["STRESS"]
