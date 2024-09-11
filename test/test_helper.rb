329dd8c604507da19f20da22d7fcccc16ae65d05
Andrew Kane <andrew@ankane.org>
2024-09-11 16:19:48 -0700

    Fixed CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ require "tmpdir"
 class Minitest::Test
   def setup
     GC.stress = true if stress?
+
+    # avoid hanging from too many threads on CI
+    GC.start if linux? && ci?
   end
 
   def teardown
@@ -26,8 +29,16 @@ class Minitest::Test
     RUBY_PLATFORM != "java"
   end
 
+  def linux?
+    RbConfig::CONFIG["host_os"] =~ /linux/i
+  end
+
   def mac?
     RbConfig::CONFIG["host_os"] =~ /darwin/i
+  end
+
+  def ci?
+    ENV["CI"]
   end
 
   def stress?
